cf6aad7303355f3635084ff97e76fb3117fc1429
Daberechukwu Nwafor <daberechukwu.nwafor@konga.com>
2023-04-03 11:34:33 +0000

    Fix: updated category update type

--- app/categories/dto/update-category-status.dto.ts
+++ app/categories/dto/update-category-status.dto.ts
@@ -1,7 +1,8 @@
 import { IsNotEmpty, IsNumber } from "class-validator";
+import { CategoryStatusEnum } from "../entities/category.entity";
 
 export class UpdateCategoryStatusDto {
   @IsNotEmpty()
   @IsNumber()
-  readonly status: number;
+  readonly status: CategoryStatusEnum;
 }
