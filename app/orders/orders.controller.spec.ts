e215c807f337f6e2b8c15a12c4eb26ac0b8f24f9
daberechukwu <daberechukwu.nwafor@konga.com>
2023-05-10 18:49:33 +0100

    Feat: added test cases

---
+++ app/orders/orders.controller.spec.ts
@@ -0,0 +1,63 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { createOrdersMock, getAllOrdersResponseMock, getOrderResponseMock } from "./orders.mock";
+import { OrdersService } from "./orders.service";
+import { OrdersController } from "./orders.controller";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { Orders } from "./entities/order.entity";
+import { MagentoRepository } from "../magento/magento.repository";
+
+describe("OrderController", () => {
+  let controller: OrdersController;
+
+  const mockOrderRepository = {
+    create: jest.fn().mockImplementation((dto) => dto),
+    save: jest.fn().mockImplementation((orderData) =>
+      Promise.resolve({
+        id: 1,
+        ...orderData,
+      })
+    ),
+    createQueryBuilder: jest.fn(() => ({
+      where: jest.fn(() => ({
+        getMany: jest.fn(),
+      })),
+    })),
+    find: jest.fn().mockImplementation(() => Promise.resolve(getAllOrdersResponseMock.data)),
+    findOne: jest.fn().mockImplementation(() => Promise.resolve(getOrderResponseMock.data)),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [OrdersController],
+      providers: [
+        OrdersService,
+        MagentoRepository,
+        {
+          provide: getRepositoryToken(Orders),
+          useValue: mockOrderRepository,
+        },
+      ],
+    }).compile();
+
+    controller = module.get<OrdersController>(OrdersController);
+  });
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe("Create an order", () => {
+    it("should create an order successfully", async () => {
+      expect(await controller.createOrder({})).toStrictEqual(createOrdersMock);
+    });
+  });
+
+  describe("Get single order", () => {
+    it("should retrieve an order successfully", async () => {
+      const id = "1";
+      expect(await controller.findSingleOrder(id)).toStrictEqual(getOrderResponseMock);
+    });
+  });
+});
