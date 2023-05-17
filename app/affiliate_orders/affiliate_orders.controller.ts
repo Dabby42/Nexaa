3332f281b535b324a8f019cfa5b3146927c0ce2b
James Momoh <james.momoh@konga.com>
2023-05-17 11:15:23 +0100

    adding swagger tags

--- app/affiliate_orders/affiliate_orders.controller.ts
+++ app/affiliate_orders/affiliate_orders.controller.ts
@@ -3,11 +3,16 @@ import { JwtGuard } from "app/auth/auth.jwt.guard";
 import { AffiliateOrdersService } from "./affiliate_orders.service";
 import { CreateAffiliateOrderDto } from "./dto/create-affiliate_order.dto";
 import { sendSuccess } from "../utils/helpers/response.helpers";
+import { AdminGuard } from "app/admin/admin.guard";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger"
 
+@ApiTags("Affiliates")
+@ApiBearerAuth("jwt")
 @Controller("affiliate")
 export class AffiliateOrderController {
   constructor(private readonly AffiliateOrdersService: AffiliateOrdersService) {}
 
+  @UseGuards(JwtGuard, AdminGuard)
   @Post()
   async createAffiliateOrder(@Body() createAffiliateOrderDto: CreateAffiliateOrderDto) {
     await this.AffiliateOrdersService.createAffiliateOrder(createAffiliateOrderDto);
