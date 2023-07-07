fe75bddf484db838db45a02f79d09e766f7cee80
James Momoh <james.momoh@konga.com>
2023-07-08 00:33:20 +0100

    Migration file formatting

--- app/db/migrations/1688772427446-BannerEntityUpdate.ts
+++ app/db/migrations/1688772427446-BannerEntityUpdate.ts
@@ -1,20 +1,19 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class BannerEntityUpdate1688772427446 implements MigrationInterface {
-    name = 'BannerEntityUpdate1688772427446'
+  name = "BannerEntityUpdate1688772427446";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`banner\` ADD \`banner_description\` varchar(255) NULL`);
-        await queryRunner.query(`ALTER TABLE \`banner\` ADD \`banner_code\` varchar(255) NULL`);
-        await queryRunner.query(`ALTER TABLE \`banner\` ADD \`campaign_start_date\` datetime NULL`);
-        await queryRunner.query(`ALTER TABLE \`banner\` ADD \`campaign_end_date\` datetime NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`banner\` ADD \`banner_description\` varchar(255) NULL`);
+    await queryRunner.query(`ALTER TABLE \`banner\` ADD \`banner_code\` varchar(255) NULL`);
+    await queryRunner.query(`ALTER TABLE \`banner\` ADD \`campaign_start_date\` datetime NULL`);
+    await queryRunner.query(`ALTER TABLE \`banner\` ADD \`campaign_end_date\` datetime NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`campaign_end_date\``);
-        await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`campaign_start_date\``);
-        await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`banner_code\``);
-        await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`banner_description\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`campaign_end_date\``);
+    await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`campaign_start_date\``);
+    await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`banner_code\``);
+    await queryRunner.query(`ALTER TABLE \`banner\` DROP COLUMN \`banner_description\``);
+  }
 }
